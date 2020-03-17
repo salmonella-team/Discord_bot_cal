273fa8130eae3386c8479909e55a435bba88dc91
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-03-17 20:28:29 +0900

    ホワイトリスト機能の実装

--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -1,11 +1,19 @@
+import * as dotenv from 'dotenv'
+
 const GoogleSpreadsheetAsPromised = require('google-spreadsheet-as-promised')
 
-const CREDS = require('../asset/google-generated-creds')
+// GoogleSpreadSheetで使う定数を定義
+dotenv.config()
+const CREDS = require('../assets/google-generated-creds.json')
+const SHEET_ID = process.env.SHEET_ID
 
-
-export const getWhiteList = async (): Promise<string[]> => {
+/**
+ * GoogleSpreadSheetからホワイトリストを取得する
+ * @return ホワイトリストの配列
+ */
+export const GetWhiteList = async (): Promise<string[]> => {
   const sheet = new GoogleSpreadsheetAsPromised()
   await sheet.load(SHEET_ID, CREDS)
-  const worksheet = await sheet.getWorksheetByName('シート1')
+  const worksheet = await sheet.getWorksheetByName('ホワイトリスト')
   return (await worksheet.getCells('A2:A100')).getAllValues().filter((v: string) => v)
 }
